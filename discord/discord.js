d7fb1fe495e02137e45624b995f66ebfe25e6ff9
Tyler Twining <twiningtyler@gmail.com>
2021-01-04 16:42:29 -0600

    Implement logging for side chats Base code for channelUpdate/channelDelete

--- discord/discord.js
+++ discord/discord.js
@@ -21,12 +21,39 @@ client.once('ready', () => {
     console.log(`Bot has started with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
 });
 
+const logMessage = message => {
+    message.gb.guild.channel(message.channel.id, message.channel).then(channel => {
+        if (channel.parentId !== null) {
+            con.query("insert into messages (id, channel, parent_channel, content, sent) values (?, ?, ?, ?, ?);", [message.id, message.channel.id, channel.parentId, message.content, message.createdAt], err => {
+                if (err) console.error(err);
+            });
+
+            con.query("select id from channels where side_listener = true and guild_id = ?;", [message.guild.id], (err, res) => {
+                if (err) {console.error(err);return;}
+
+                const spchMsg = `:speech_balloon: **${message.author.tag}** in #${message.channel.name}: \`${message.content}\``;
+
+                res.forEach(channelObj => {
+                    message.gb.guild.channel(channelObj.id).then(listenerChannel => {
+                        listenerChannel.discord.send(spchMsg);
+                    });
+                });
+
+                console.log(`(#${message.channel.name}) ${message.author.tag}: ${message.content}`);
+            });
+        }
+    }).catch(console.error);
+}
+
 // implement commands
 client.on('message', async message => {
     message.bot_id = client.user.id;
 
-    // if the message doesn't start with the prefix, or the person talking is the bot, we don't need to process this.
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    // we can't have messages with no guild attached after this point
+    if (message.guild === undefined || message.guild === null) return;
+
+    // if the message is sent by a bot, we don't need process this at all.
+    if (message.author.bot) return;
 
     const Manager = new GB.Manager(client);
     Manager.guild(message.guild.id, message.guild).then(gbguild => {
@@ -34,6 +61,11 @@ client.on('message', async message => {
             Manager: Manager,
             guild: gbguild,
         };
+
+        logMessage(message);
+
+        // if the message doesn't start with the prefix we don't need to process this as a command.
+        if (!message.content.startsWith(prefix)) return;
 
         const args = message.content.slice(prefix.length).trim().split(/ +/);
         const command = args.shift().toLowerCase();
@@ -63,53 +95,55 @@ const updateChannelPermissions = channel => {
 
     Manager.guild(channel.guild.id, channel.guild).then(gbguild => {
         gbguild.channel(channel.id, channel).then(gbChannel => {
-            if (channel.members.array().length > 0) {
-                gbChannel.side().then(sideChannel => {
-                    let permissions = [{id: client.user, allow: ['VIEW_CHANNEL']}, {id: channel.guild.roles.cache.find(role => role.name === "@everyone"), deny: ['VIEW_CHANNEL']}];
+            if (gbChannel.create_side) {
+                if (channel.members.array().length > 0) {
+                    gbChannel.side().then(sideChannel => {
+                        let permissions = [{id: client.user, allow: ['VIEW_CHANNEL']}, {id: channel.guild.roles.cache.find(role => role.name === "@everyone"), deny: ['VIEW_CHANNEL']}];
 
-                    channel.members.each(member => {
-                        permissions = [
-                            {
-                                id: member,
-                                allow: ['VIEW_CHANNEL']
-                            },
-                            ...permissions
-                        ];
-                    });
+                        channel.members.each(member => {
+                            permissions = [
+                                {
+                                    id: member,
+                                    allow: ['VIEW_CHANNEL']
+                                },
+                                ...permissions
+                            ];
+                        });
 
-                    if (sideChannel === null) {
-                        let sideChannelName = channel.name.toLowerCase().replace(/[^ 0-9a-z-]/g, "").replace(/ +/g, "-") + "-side";
-                        console.log("Creating side channel " + sideChannelName);
-                        channel.guild.channels.create(sideChannelName, {
-                            type: "text",
-                            parent: channel.parent,
-                            permissionOverwrites: permissions,
-                        }).then(newSideChannel => {
-                            con.query("insert into channels (id, guild_id, parent) values (?, ?, ?);", [newSideChannel.id, channel.guild.id, channel.id], err => {
-                                if (err) {console.error(err);return;}
+                        if (sideChannel === null) {
+                            let sideChannelName = channel.name.toLowerCase().replace(/[^ 0-9a-z-]/g, "").replace(/ +/g, "-") + "-side";
+                            console.log("Creating side channel " + sideChannelName);
+                            channel.guild.channels.create(sideChannelName, {
+                                type: "text",
+                                parent: channel.parent,
+                                permissionOverwrites: permissions,
+                            }).then(newSideChannel => {
+                                con.query("insert into channels (id, guild_id, parent) values (?, ?, ?);", [newSideChannel.id, channel.guild.id, channel.id], err => {
+                                    if (err) {console.error(err);return;}
 
-                                con.query("update channels set side = ? where id = ?;", [newSideChannel.id, channel.id], err => {
-                                    if (err) console.error(err);
+                                    con.query("update channels set side = ? where id = ?;", [newSideChannel.id, channel.id], err => {
+                                        if (err) console.error(err);
+                                    });
                                 });
-                            });
-                        }).catch(console.error);
-                    } else {
-                        sideChannel.discord.overwritePermissions(permissions);
-                    }
-                }).catch(err => {
-                    console.log(err);
-                });
-            } else {
-                gbChannel.side().then(sideChannel => {
-                    if (sideChannel !== null) {
-                        sideChannel.discord.delete();
+                            }).catch(console.error);
+                        } else {
+                            sideChannel.discord.overwritePermissions(permissions);
+                        }
+                    }).catch(err => {
+                        console.log(err);
+                    });
+                } else {
+                    gbChannel.side().then(sideChannel => {
+                        if (sideChannel !== null) {
+                            sideChannel.discord.delete();
 
-                        con.query("delete from channels where id = ?;", [sideChannel.discord.id]);
-                        con.query("update channels set side = null where side = ?;", [sideChannel.discord.id]);
-                    }
-                }).catch(err => {
-                    console.log(err);
-                });
+                            con.query("delete from channels where id = ?;", [sideChannel.discord.id]);
+                            con.query("update channels set side = null where side = ?;", [sideChannel.discord.id]);
+                        }
+                    }).catch(err => {
+                        console.log(err);
+                    });
+                }
             }
         }).catch(console.error);
     }).catch(console.error);
@@ -129,6 +163,18 @@ client.on("voiceStateUpdate", async (oldState, newState) => {
             updateChannelPermissions(toChannel);
         }
     }
+});
+
+client.on("channelUpdate", (oldChannel, newChannel) => {
+    if (newChannel.name === "disallow") {
+        
+    }
+});
+
+client.on("channelDelete", channel => {
+    con.query("delete from channels where id = ?;", [channel.id], err => {
+        if (err) console.error(err);
+    });
 });
 
 console.log("Logging in...");
